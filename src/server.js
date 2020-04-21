000c5382af3f96cf07dda1ac3d9985aff5c5a82e
chaesh <csh1708@gmail.com>
2020-04-21 16:42:38 +0900

    #3.11 editUser and seeUser

--- src/server.js
+++ src/server.js
@@ -5,12 +5,13 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { prisma } from "../generated/prisma-client";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request }),
+  context: ({ request }) => ({ request, isAuthenticated }),
 });
 
 server.express.use(logger("dev"));
